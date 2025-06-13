14d6fccb951ebfd1b840b39786b3d6599762f5b8
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-06-13 11:14:16 -0700

    adds solution

--- pairings.py
+++ pairings.py
@@ -1,7 +1,23 @@
 def find_pairs(num_string):
-  # Write your solution here!
-  pass
+  # Write your solution here!/
+  # split each items in string/
+  # convert elemnt to num/
+  # find a pairs:
+  nums = []
+  for num in num_string.split():
+    num = int(num)
+    nums.append(num)
+  
+  pairs = set()
+  
+  for num in nums:
+    for j in nums:
+      if num < j:
+        pairs.add((num, j))
+  return pairs
+  
 
+# print(find_pairs("7 3 99"))
 
 # Test cases
 assert find_pairs("7 3 99") == {(7, 99), (3, 7), (3, 99)}
